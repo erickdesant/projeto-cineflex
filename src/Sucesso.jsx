173c062f56047f02c97b0aa8c587cae9666ed545
Erick Santana <ericksantana2009@gmail.com>
2024-09-05 12:10:28 -0300

    bug

--- src/Sucesso.jsx
+++ src/Sucesso.jsx
@@ -32,7 +32,8 @@ export default function Sucesso(){
 
 const BodyContainer = styled.div`
     width: 100%;
-    height: 100%;
+    min-height: 100vh;
+    max-height: 100%;
     background-color: #212226;
     display: flex;
     align-items: center;
